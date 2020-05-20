e6fcf44aff1780ddb1bf595546732e1c2d4d3a70
jhaydari72@gmail.com <jhaydari72@gmail.com>
2020-05-20 11:36:31 -0700

    first pass animations (CHARC)

--- Fool's Gold/Godot Game Projects/Fool's Gold/KinematicBody2D.gd
+++ Fool's Gold/Godot Game Projects/Fool's Gold/KinematicBody2D.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 50
 
 var velocity = Vector2()
+onready var sprite = $Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -12,12 +13,16 @@ func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_d"):
 		velocity.x += 1
+		$Sprite.play("right_walk")
 	if Input.is_action_pressed("ui_a"):
 		velocity.x -= 1
+		$Sprite.play("left_walk")
 	if Input.is_action_pressed("ui_s"):
 		velocity.y += 1
+		$Sprite.play("down_walk")
 	if Input.is_action_pressed("ui_w"):
 		velocity.y -= 1
+		$Sprite.play("up_walk")
 	velocity = velocity.normalized() * speed
 
 
