8787169870ca974e97acb2b3015b61955ec330bd
jhaydari72@gmail.com <jhaydari72@gmail.com>
2020-05-28 18:43:37 -0700

    added notes for script

--- Fool's Gold/Godot Game Projects/Fool's Gold/KinematicBody2D.gd
+++ Fool's Gold/Godot Game Projects/Fool's Gold/KinematicBody2D.gd
@@ -1,26 +1,29 @@
 extends KinematicBody2D
-
+#https://www.youtube.com/watch?v=Z9aR9IiiHT8
+#this tutorial is the crux of the movement and sprite changing
 const ACCELERATION = 500
 const MAX_SPEED = 50
 const FRICTION = 500
 
 var Key = 0
 var velocity = Vector2.ZERO
-
+#these signal the different nodes for specific purposes
 onready var player_anim = $player_anim
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 
-
+#this function is called every frame of the game
 func _physics_process(delta):
 	
 	var input_vector = Vector2.ZERO
 	
+	#movement
 	input_vector.x = Input.get_action_strength("ui_d") - Input.get_action_strength("ui_a")
 	input_vector.y = Input.get_action_strength("ui_s") - Input.get_action_strength("ui_w")
 	input_vector = input_vector.normalized()
 	
+	# gives speed and animations
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
